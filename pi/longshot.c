1ae0fc238ca8ec28b63f095aa8b5f4327d34464e
Paul Reynolds <paul@blastoffgames.net>
2013-05-24 16:51:00 -0400

    Getting makefile ready

--- pi/longshot.c
+++ pi/longshot.c
@@ -1,27 +1,3 @@
-/*
- * serialTest.c:
- *	Very simple program to test the serial port. Expects
- *	the port to be looped back to itself
- *
- * Copyright (c) 2012-2013 Gordon Henderson. <projects@drogon.net>
- ***********************************************************************
- * This file is part of wiringPi:
- *	https://projects.drogon.net/raspberry-pi/wiringpi/
- *
- *    wiringPi is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Lesser General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    wiringPi is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public License
- *    along with wiringPi.  If not, see <http://www.gnu.org/licenses/>.
- ***********************************************************************
- */
 
 #include <stdio.h>
 #include <string.h>
@@ -36,7 +12,7 @@ int main ()
   int count ;
   unsigned int nextTime ;
 
-  if ((fd = serialOpen ("/dev/ttyAMA0", 115200)) < 0)
+  if ((fd = serialOpen ("/dev/ttyUSB0", 57600)) < 0)
   {
     fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
     return 1 ;
