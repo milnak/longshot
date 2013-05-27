a990784b671629a65041a7feb224b86692c65ada
Paul Reynolds <paul@blastoffgames.net>
2013-05-27 15:27:08 -0400

    Working on getting the in and out game data formatted

--- pi/longshot.c
+++ pi/longshot.c
@@ -6,46 +6,88 @@
 #include <wiringPi.h>
 #include <wiringSerial.h>
 
+enum SwitchBits {
+ SWITCH_FREEGAMELIGHT,
+ SWITCH_GAMEOVERLIGHT,
+ SWITCH_WINNERLIGHT,
+ SWITCH_BEACONLIGHT,
+ SWITCH_COINMETER,
+ SWITCH_TICKETMETER,
+ SWITCH_SOLENOID,
+ SWITCH_TICKETDISPENSER
+};
+ 
+
+typedef struct GameState {
+  unsigned char switches;
+  unsigned char dispense;
+  unsigned char score;
+  unsigned char ballCount;
+  unsigned char _terminator;
+} outGameState;
+
+typedef struct GameStatus
+{
+  unsigned int ticketsDispensed;
+  unsigned int scoreClicks;
+  unsigned int hundredClicks;
+  unsigned int ballClicks;
+  unsigned int coinClicks;
+  unsigned int upClicks;
+  unsigned int downClicks;
+  unsigned int selectClicks;
+  unsigned int setupClicks;
+} inGameState;
+
 int main ()
 {
-  int fd ;
-  int count ;
-  unsigned int nextTime ;
+  int fd;
+  unsigned int nextTime;
 
+  outGameState._terminator = "\0";
+
+  // open our USB connection
   if ((fd = serialOpen ("/dev/ttyUSB0", 57600)) < 0)
   {
     fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
     return 1 ;
   }
 
+  // see if wiringPi is DTF
   if (wiringPiSetup () == -1)
   {
     fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
     return 1 ;
   }
 
-  nextTime = millis () + 300 ;
+  // kick off the update loop
+  nextTime = millis () + 300;
 
-  for (count = 0 ; count < 256 ; )
+  while (true)
   {
     if (millis () > nextTime)
     {
-      printf ("\nOut: %3d: ", count) ;
-      fflush (stdout) ;
-      serialPutchar (fd, count) ;
+      // write our requests
+      unsigned char* outStateMem = &outGameState;
+
+      for (int i = 0; i < sizeof(outGameState); i++, outStateMem++)
+        serialPutchar(fd, outStateMem);
+
+      serialFlush( fd )
+
+      // read the data from the arduino
+      unsigned char* inStateMem = &inGameState;
+      while (serialDataAvail (fd))
+      {
+         *inStateMem = serialGetchar(fd);
+         inStateMem += sizeof(unsigned char); // we're only reading a byte at a time
+      }
+      
       nextTime += 300 ;
-      ++count ;
     }
 
     delay (3) ;
-
-    while (serialDataAvail (fd))
-    {
-      printf (" -> %3d", serialGetchar (fd)) ;
-      fflush (stdout) ;
-    }
   }
 
-  printf ("\n") ;
   return 0 ;
 }
