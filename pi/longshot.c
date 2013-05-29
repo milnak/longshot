a6f960e15b5136a4d4b54be4464db6357179f641
Mikkel Green <ackbar345@hotmail.com>
2013-05-29 17:56:52 -0400

    Increasingly random...

--- pi/longshot.c
+++ pi/longshot.c
@@ -2,11 +2,11 @@
 #include "longshot.h"
 
 void InitLongshot() {
-    gMachineOut.dispense  = 0;
+    //gMachineOut.dispense  = 0;
 }
 
 void UpdateLongshot() {
-    gMachineOut.dispense  = 0;
+    //gMachineOut.dispense  = 0;
      if (gMachineInPrev.hundredClicks < gMachineIn.hundredClicks)
     {
         gMachineOut.score += (50 * (gMachineIn.hundredClicks - gMachineInPrev.hundredClicks));
